aad5b9bf794e14b4d66553fc6486538ee227bbee
Alif Dwi Rahman <alifdwirahman54@gmail.com>
2025-04-04 15:54:00 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+      <h2>Hello World</h2>
+    </div>
+  );
+}
+
+export default App;
